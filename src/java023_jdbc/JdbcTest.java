0be986676535de2ece8f10d7f6a5fda70deb4483
txwxkx <txwxkx@gmail.com>
2023-01-10 15:16:28 +0900

    230110 | add fixed

--- src/java023_jdbc/JdbcTest.java
+++ src/java023_jdbc/JdbcTest.java
@@ -27,14 +27,51 @@ public class JdbcTest {
 			String password = "a1234";
 			conn = DriverManager.getConnection(url, username, password);
 			
-			System.out.println(conn);
+			//System.out.println(conn);
+			
+			//3. 쿼리문을 실행하기 위한 Statement 타입의 객체를 가져옴
+			stmt = conn.createStatement();
+			
+			//4. 쿼리문을 실행 : SELECT - executeQuery(), INSERT, UPDATE, DELETE - executeUpdate()
+			String sql = "SELECT * FROM departments ORDER BY department_id";
+			rs = stmt.executeQuery(sql);
+			//System.out.println(rs);
+			
+			//5. 쿼리문의 결과 출력
+			while(rs.next()) {
+				int departmentId = rs.getInt("department_id");
+				String departName = rs.getString("department_name");
+				int managerId = rs.getInt("manager_id");
+				int locationId = rs.getInt("location_id");
+				System.out.printf("%d %s %d %d\n",
+						departmentId, departName, managerId, locationId);
+			}
 			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-
+			if(rs!=null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			
+			if(stmt != null)
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			
+			if(conn != null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 		}
 
 	}
